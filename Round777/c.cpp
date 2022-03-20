e32d498d31015f578fd00800c48ffa3f3b086e34
omkar-mohanty <franzohouser@gmail.com>
2022-03-20 09:14:43 +0530

    Round777: Solution for c

--- Round777/c.cpp
+++ Round777/c.cpp
@@ -1,62 +1,57 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " " << x << endl;
-using ull = unsigned long long;
-using ll = long long;
+
 using namespace std;
+
 void solve()
 {
     int n, m;
-    string s;
-    string ans = "";
     cin >> n >> m;
-    int a[n][m];
-    for (int i = 0; i < n; i++)
+    vector<vector<int>> a(n, vector<int>(m));
+    for (int i = 0; i < n; ++i)
     {
+        string s;
         cin >> s;
-        for (int j = 0; j < m; j++)
-        {
+        for (int j = 0; j < m; ++j)
             a[i][j] = s[j] - '0';
-        }
     }
+    vector<array<int, 4>> ans;
     if (a[0][0] == 1)
     {
-        cout << "-1\n";
+        cout << -1 << '\n';
         return;
     }
 
-    cout << n * m - 1 << "\n";
-    for (int i = 1; i < n; i++)
+    for (int j = m - 1; j >= 0; --j)
     {
-        for (int j = 0; j < n; j++)
+        for (int i = n - 1; i >= 0; --i)
         {
-            if (a[i][j] == 1)
+            if (a[i][j])
             {
-                if (j == 0)
+                if (i != 0)
                 {
-                    cout << i << " " << j + 1 << " " << i + 1 << " " << j + 1 << "\n";
+                    ans.push_back({i, j + 1, i + 1, j + 1});
                 }
                 else
                 {
-                    cout << i + 1 << " " << j + 1 << " " << i + 1 << " " << j << "\n";
+                    ans.push_back({i + 1, j, i + 1, j + 1});
                 }
             }
         }
+    }
+    cout << ans.size() << '\n';
+    for (auto i : ans)
+    {
+        cout << i[0] << ' ' << i[1] << ' ' << i[2] << ' ' << i[3] << '\n';
     }
 }
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
-    int t = 1;
+
+    int t;
     cin >> t;
     while (t--)
-    {
         solve();
-    }
 }
